c849f5bd3f8333da1aac5b8cab2597edf2429b9d
Ahmed Mohamed Fareed <Coder_Mac@Ahmeds-Mac.local>
2013-08-28 07:17:18 +0200

    Initial Commit

---
+++ JTAP/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  JTAP
+//
+//  Created by Ahmed Mohamed Fareed on 8/28/13.
+//  Copyright (c) 2013 Ahmed Mohamed Fareed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ JTAP/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  JTAP
+//
+//  Created by Ahmed Mohamed Fareed on 8/28/13.
+//  Copyright (c) 2013 Ahmed Mohamed Fareed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
